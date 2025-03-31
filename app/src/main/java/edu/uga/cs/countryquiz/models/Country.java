0e09e4ea7e8c79bd429ac50407d5142d7227b470
Isaias Pavon <isaiaspavon22@gmail.com>
2025-03-31 01:14:09 -0400

    POJO for Country created (schema for Country)

--- app/src/main/java/edu/uga/cs/countryquiz/models/Country.java
+++ app/src/main/java/edu/uga/cs/countryquiz/models/Country.java
@@ -1,4 +1,30 @@
 package edu.uga.cs.countryquiz.models;
 
 public class Country {
+
+    private int id;
+    private String name;
+    private String continent;
+
+    // Constructor
+    public Country(int id, String name, String continent) {
+        this.id = id;
+        this.name = name;
+        this.continent = continent;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    // Optionally, you can add setters if needed
 }
