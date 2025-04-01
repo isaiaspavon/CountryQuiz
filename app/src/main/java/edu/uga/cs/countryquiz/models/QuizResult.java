0a08b2c5038a88a79863c1812fc75b09bd624819
Isaias Pavon <isaiaspavon22@gmail.com>
2025-04-01 11:16:12 -0400

    QuizResult POJO created

--- app/src/main/java/edu/uga/cs/countryquiz/models/QuizResult.java
+++ app/src/main/java/edu/uga/cs/countryquiz/models/QuizResult.java
@@ -1,4 +1,25 @@
 package edu.uga.cs.countryquiz.models;
 
 public class QuizResult {
+    private int id;
+    private String date;
+    private int score;
+
+    public QuizResult(int id, String date, int score) {
+        this.id = id;
+        this.date = date;
+        this.score = score;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
