c2aec18d5eee2be36669fcd137123abf6b61c8bd
Isaias Pavon <isaiaspavon22@gmail.com>
2025-04-03 22:01:36 -0400

    AsyncCSVLoader javadocs created

--- app/src/main/java/edu/uga/cs/countryquiz/AsyncCSVLoader.java
+++ app/src/main/java/edu/uga/cs/countryquiz/AsyncCSVLoader.java
@@ -9,16 +9,33 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+/**
+ * An asynchronous task that loads country and continent data from a CSV file
+ * into the SQLite database. It ensures that the database is not repopulated
+ * if data already exists.
+ */
 public class AsyncCSVLoader extends AsyncTask<Void, Void, Void> {
 
     private Context context;
     private DatabaseHelper dbHelper;
 
+    /**
+     * Constructs an AsyncCSVLoader instance.
+     *
+     * @param context The application context used for accessing resources and the database.
+     */
     public AsyncCSVLoader(Context context) {
         this.context = context;
         this.dbHelper = DatabaseHelper.getInstance(context);
     } // AsyncCSVLoader
 
+    /**
+     * Loads the CSV data into the database in the background.
+     * If the database is already populated, it skips the loading process.
+     *
+     * @param voids Unused parameter.
+     * @return null
+     */
     @Override
     protected Void doInBackground(Void... voids) {
         // Check if database already has data
@@ -62,7 +79,11 @@ public class AsyncCSVLoader extends AsyncTask<Void, Void, Void> {
         return null;
     } // doInBackground
 
-    // Check if the database already contains data
+    /**
+     * Checks whether the database is already populated with country data.
+     *
+     * @return true if the database contains country data, false otherwise.
+     */
     private boolean isDatabasePopulated() {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         Cursor cursor = db.rawQuery("SELECT COUNT(*) FROM " + DatabaseHelper.TABLE_COUNTRIES, null);
