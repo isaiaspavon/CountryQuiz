aa03daa828f7060d58c9211680a846a153f42ce1
Isaias Pavon <isaiaspavon22@gmail.com>
2025-04-03 22:11:19 -0400

    QuizAdapter javadocs created

--- app/src/main/java/edu/uga/cs/countryquiz/QuizAdapter.java
+++ app/src/main/java/edu/uga/cs/countryquiz/QuizAdapter.java
@@ -7,15 +7,31 @@ import androidx.viewpager2.adapter.FragmentStateAdapter;
 import java.util.List;
 import edu.uga.cs.countryquiz.models.Country;
 
+/**
+ * QuizAdapter is a ViewPager2 adapter that manages the quiz questions.
+ * It creates a new QuizFragment for each question in the quiz.
+ */
 public class QuizAdapter extends FragmentStateAdapter {
 
     private List<Country> quizCountries;
 
+    /**
+     * Constructs a QuizAdapter.
+     *
+     * @param activity The MainActivity that hosts the ViewPager2.
+     * @param quizCountries The list of Country objects used for the quiz.
+     */
     public QuizAdapter(MainActivity activity, List<Country> quizCountries) {
         super(activity);
         this.quizCountries = quizCountries;
-    }
+    } // QuizAdapter
 
+    /**
+     * Creates a QuizFragment for the given position (question index).
+     *
+     * @param position The index of the question.
+     * @return A new instance of QuizFragment with the question index set as an argument.
+     */
     @Override
     public Fragment createFragment(int position) {
         // Create a new QuizFragment instance for the current position (question index)
@@ -24,10 +40,16 @@ public class QuizAdapter extends FragmentStateAdapter {
         QuizFragment fragment = new QuizFragment();
         fragment.setArguments(args);
         return fragment;
-    }
+    } // createFragment
 
+    /**
+     * Returns the total number of questions in the quiz.
+     *
+     * @return The number of quiz questions.
+     */
     @Override
     public int getItemCount() {
         return quizCountries.size();
-    }
-}
+    } // getItemCount
+
+} // QuizAdapter
